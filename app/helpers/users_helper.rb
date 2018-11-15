6ee64831a6ffad97fcf1cb735ac4b74dea10f69a
naruru <dunshididao295@gmail.com>
2018-11-15 16:11:54 +0000

    user login

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,11 @@
+module UsersHelper
+  def gravatar_url(user, options={size: 80})
+    garavatar_id= Digest::MD5::hexdigest(user.email.downcase)
+    size= options[:size]
+    "https://secure.garavatar.com/avatar/
+#{garavatar_id}
+?s=
+#{size}
+"
+  end
+end
